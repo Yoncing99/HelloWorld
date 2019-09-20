c0d072b311d9c85e58802cffc5ee0420a27f24f5
Goh Yon Cing <55395418+Yoncing99@users.noreply.github.com>
2019-09-20 15:56:54 +0800

    version 1

---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+public class HelloWorld{
+
+    public static void main(String[]args){
+
+        double A = 3*6*0.5;
+        double B = 3*4*0.5;
+        double C = 4*3;
+
+        System.out.println("A = " + 3*6*0.5);
+        System.out.println("B = " + 3*4*0.5);
+        System.out.println("A = " + 4*3);
+        System.out.println("Total = " + (A+B+C));
+
+
+    }
+
+
+
+
+
+
+
+}
